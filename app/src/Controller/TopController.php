f29841d0eb1d15e31ba0b8c6564e8874312a1fa7
yu <yuu2dev@gmail.com>
2019-10-19 22:02:05 +0900

    Add Template : About me, Contact me

--- app/src/Controller/TopController.php
+++ app/src/Controller/TopController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\CategoryHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,17 +24,24 @@ class TopController extends AbstractController implements ApplicationController 
     /**
      * 메인
      * @access public
-     * @param CategoryHelper $categoryHelper
      * @Route("/", name="home")
      * @Template("home.twig")
      */
     public function main() {
       $em = $this->entityManager;
-      // $aside = new AsideService($em);
+      
+      return array();
+    }
 
-      return array(
-        // 'Aside' => $aside->execute()
-      );
+    /**
+     * 자기 소개
+     * @access public
+     * @Route("/about", name="about")
+     * @Template("about.twig")
+     */
+    public function about() {
+
+      return array();
     }
 
     /**
@@ -49,19 +55,16 @@ class TopController extends AbstractController implements ApplicationController 
       return array();
     }
 
-
-    // /**
-    //  * @access public
-    //  * @Route("/about", name="about")
-    //  */
-    // function about() {
-
-    //   $entityManager = $this->getDoctrine()->getManager();
-    //   $aside = new AsideService($entityManager);
-    //   $data['Aside'] = $aside->execute();
-
-    //   return $this->render('about.html.twig', $data);
-    // }
+    /**
+     * 방명록
+     * @access public
+     * @Route("/guestbook", name="guestbook")
+     * @Template("/guestbook.twig")
+     */
+    public function guestbook() {
+      
+      return array();
+    }
 }
 
 ?>
