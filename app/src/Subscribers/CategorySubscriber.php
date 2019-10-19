69d2e3ccfb2ea4edaff8961fd107f36176ad8260
yu <myco8332v2@gmail.com>
2019-10-20 00:39:20 +0900

    전처리 이벤트 설계

--- app/src/Subscribers/CategorySubscriber.php
+++ app/src/Subscribers/CategorySubscriber.php
@@ -2,14 +2,28 @@
 
 namespace App\Subscribers;
 
+use App\Repository\CategoryRepository;
+
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class CategorySubscriber implements EventSubscriberInterface {
 
   /**
+   * @var CategoryRepository
+   */
+  private $categoryRepository;
+
+  /**
    * @access public
    */
-  public function __construct() {}
+  public function __construct(
+    CategoryRepository $categoryRepository
+  ) {
+    $this->categoryRepository = $categoryRepository;  
+  }
 
   /**
    * @access public
@@ -18,15 +32,16 @@ class CategorySubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
 
     return array(
-
+      KernelEvents::RESPONSE => 'onApplicationController'
     );
   }
 
   /**
    * @access public
+   * @param ResponseEvent $event
    */
-  public function OnServeCategory() {
-    
+  public function onApplicationController(ResponseEvent $event) {
+
   }
 
 
