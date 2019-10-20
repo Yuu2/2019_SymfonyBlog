91379f6232707540b60b19e749a48a06e880bacf
yu <myco8332v2@gmail.com>
2019-10-20 21:28:59 +0900

    AccountController: 인증처리 리팩토링 / Authenticator: CSRF 액세스 토큰

---
+++ app/src/Controller/AccountController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+use ReCaptcha\ReCaptcha;
+
+/**
+ * 2019.10.20
+ * @author Yuu2
+ */
+class AccountController extends AbstractController implements ApplicationController {
+
+    /**     
+     * 로그인
+     * @access public
+     * @Route("/account/signin", name="account_signin", methods={"GET", "POST"})
+     * @Template("/account/signin.twig")
+     */
+    public function signin(AuthenticationUtils $authenticationUtils) {
+        
+      if($this->getUser()) {$this->redirectToRoute('account_signin');}
+
+      $error = $authenticationUtils->getLastAuthenticationError();
+
+      $lastUsername = $authenticationUtils->getLastUsername();
+
+      return  array (
+        'last_username' => $lastUsername, 
+        'error' => $error
+      );
+    }
+
+    /**
+     * 로그아웃
+     * 
+     * @access public
+     * @Route("/account/signout", name="account_signout")
+     */
+    public function signout() {
+        throw new \Exception();
+    }
+}
