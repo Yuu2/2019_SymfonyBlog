7cd431601890e0c235b1e33975b0a23fc789a5a0
yu <yuu2dev@gmail.com>
2019-12-21 00:00:14 +0900

    Remove Template : unnecessary elements of sourcecode

--- app/public/style/js/headroom-active.js
+++
@@ -1,9 +0,0 @@
-/*-----------------------------
- intelligent-header
----------------------------- */	
-// grab an element
-var myElement = document.querySelector(".intelligent-header");
-// construct an instance of Headroom, passing the element
-var headroom  = new Headroom(myElement);
-// initialise
-headroom.init(); 
