91636fe906037e47793f4ee7a85aa702b4143837
yu <yuu2dev@gmail.com>
2020-08-01 10:28:49 +0900

    Refactoring Blog - twig template & remove ck-editor bundles

--- symfony/docker-entrypoint.sh
+++ symfony/docker-entrypoint.sh
@@ -1,11 +1,13 @@
-cd ${PROJECT_DIR}
-
 composer install --no-scripts;
 
 # CKEIDTOR / ELFINDER
 if [ ! -d "${PROJECT_DIR}/public/bundles" ]; then
+  mkdir {$PROJECT_DIR}/public/bundles
+  
   bin/console ckeditor:install && bin/console assets:install public
   bin/console elfinder:install public
+
+  cp ${PROJECT_DIR}/public/plugins ${PROJECT_DIR}/public/bundles
 fi
 
 # Doctrine Migrations
