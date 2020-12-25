bcbb3bc6cb8d38e7481514edb3b136d26533d7d8
yu <yuu2dev@gmail.com>
2020-12-26 06:34:37 +0900

    관리자 베이스 템플릿 및 좌측 네비

---
+++ symfony/src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * @author yuu2dev
+ * updated 2020.12.26
+ */
+class CategoryFixtures extends AbstractFixtures implements DependentFixtureInterface {
+
+  /**
+   * @access public
+   * @param ObjectManager $objectManager
+   * @return void
+   */
+  public function load(ObjectManager $objectManager): void {
+    
+    // 일기 카테고리 추가
+    $category = new Category;
+    $category->setTitle('일기');
+    $category->setCreatedAt(new \DateTime);
+    $category->setSortNo(1);
+
+    $this->addCategory($category);
+  }
+
+  /**
+   * @access protected
+   * @param array $categories
+   * @return void
+   */
+  protected function addCategories(array $categories) {}
+  
+  /**
+   * @access protected
+   * @param Category $category
+   * @param bool $flagFlush
+   * @return void;
+   */
+  protected function addCategory(Category $category, $flagFlush = true) {
+    $this->entityManager->persist($category);
+    $flagFlush ? $this->entityManage->flush() : null;
+  }
+
+  /**
+   * @access public
+   * @return array
+   */
+  public function getDependencies(): array {
+    return [
+      UserFixtures::class
+    ];
+  }
+}
