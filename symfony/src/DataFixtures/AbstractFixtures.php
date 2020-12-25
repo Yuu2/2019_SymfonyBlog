8839c9484bbf8cfbc6697cea5dc35e28bc9720d8
yu <yuu2dev@gmail.com>
2020-12-26 08:05:45 +0900

    FontAwesome 4 -> 5 버전업

--- symfony/src/DataFixtures/AbstractFixtures.php
+++ symfony/src/DataFixtures/AbstractFixtures.php
@@ -3,9 +3,11 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Entity\Portfolio;
 use App\Entity\Skill;
 use App\Entity\Tag;
+use App\Entity\User;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -15,26 +17,9 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 /**
  * @abstract 
  * @author yuu2dev
- * updated 2020.06.30
+ * updated 2020.12.27
  */
 abstract class AbstractFixtures extends Fixture {
 
-  /**
-   * @var ObjectManager
-   */
-  protected $objectManager;
-
-  /**
-   * @var UserPasswordEncoderInterface
-   */
-  protected $userPasswordEncoder;
-
-  /**
-   * @access public
-   * @param UserPasswordEncoderInterface $userPasswordEncoder
-   */
-  public function __construct(UserPasswordEncoderInterface $userPasswordEncoder) {
-    $this->userPasswordEncoder = $userPasswordEncoder;
-  }
 }
 
